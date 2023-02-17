bbf52cddf30afea20a85c5cefdecc8604cd81f78
davidamed <amedewonudavid5@gmail.com>
2023-02-16 19:03:00 -0800

    unix system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - Prints a statement to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+        fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        return (1);
+}
