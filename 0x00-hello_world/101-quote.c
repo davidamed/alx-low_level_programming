5b3690d4fefb72e7cc0018c1fb6df8ef5f7b16f6
davidamed <amedewonudavid5@gmail.com>
2023-02-16 19:05:41 -0800

    unix system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
- * main - Prints a statement to the standard error
+ * main - Entry point
  *
- * Return: 1
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        fprintf(stderr, "\"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
