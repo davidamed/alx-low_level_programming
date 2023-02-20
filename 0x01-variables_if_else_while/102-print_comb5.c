2ad2ded3ca33b963ad5ba2302b1330d7cd25c67d
davidamed <amedewonudavid5@gmail.com>
2023-02-20 11:37:08 -0800

    two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,21 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-
-		for (j = i; j <= 99; j++)
+		for (j = i; j < 100; j++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			int a = i / 10;
+			int b = i % 10;
+			int c = j / 10;
+			int d = j % 10;
+
+			putchar(a + '0');
+			putchar(b + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
 			if (i != 99 || j != 99)
 			{
 				putchar(',');
