f86f83c3ae8da5d7fa06912ee53ef0055f8cf499
davidamed <amedewonudavid5@gmail.com>
2023-02-21 12:51:00 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints lowercase
+ * @c: parameter to be printed
+ *
+ * Return: 1 if it is a lowercase and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
